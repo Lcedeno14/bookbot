1fc0c9de71fa05a068263f94301059f4d20c42c9
Lcedeno14 <59905362+Lcedeno14@users.noreply.github.com>
2024-03-15 16:55:46 -0700

    counting letters in a book

--- main.py
+++ main.py
@@ -3,11 +3,31 @@ def openBooks(path):
         # reading frankenstein book
         file_contents = f.read()
         return file_contents
+def addAlphabetDictionary():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    letters = {}
+    for letter in alphabet:
+        letters[letter] = 0
+    return letters
+def countBook(book):
+    bookSplit = book.split()
+    #bookWords = len(bookSplit)
+    letters_dict = {}
+    for words in bookSplit:
+        for letters in words:
+            letter=letters.lower()
+            if letter in letters_dict:
+                letters_dict[letter] = letters_dict[letter] + 1
+            else:
+                letters_dict[letter] = 1
+    return letters_dict
     
 def main():
     bookPath = "books/frankenstein.txt"
     book = openBooks(bookPath)
-    print(book)
+    wordCount = countBook(book)
+    print(wordCount)
+    
 
 
 main()
