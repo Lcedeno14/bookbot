8766fae691513862bd5345ee0f72cf527e0cc8a6
Lcedeno14 <59905362+Lcedeno14@users.noreply.github.com>
2024-03-18 18:07:36 -0700

    Prints report

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ def openBooks(path):
         # reading frankenstein book
         file_contents = f.read()
         return file_contents
-def addAlphabetDictionary():
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    letters = {}
-    for letter in alphabet:
-        letters[letter] = 0
-    return letters
-def countBook(book):
+    
+def wordCount(book):  
     bookSplit = book.split()
-    #bookWords = len(bookSplit)
+    bookWords = len(bookSplit)
+    return bookWords  
+
+def countBook(book):
     letters_dict = {}
-    for words in bookSplit:
+    for words in book:
         for letters in words:
             letter=letters.lower()
             if letter in letters_dict:
@@ -21,13 +19,31 @@ def countBook(book):
             else:
                 letters_dict[letter] = 1
     return letters_dict
-    
+
+def sorter(dictionary):
+    return dictionary["num"]
+
+def sortDict(dictionary):
+    dictToList = []
+    for letters in dictionary:
+        dictToList.append({"letter":letters, "num":dictionary[letters]})
+    dictToList.sort(key = sorter,reverse = True)
+    return dictToList
+
+def printLetters(dictionary):
+    for letters in dictionary:
+        if letters["letter"].isalpha():
+            print(f"The '{letters["letter"]}' character was found {letters["num"]} times")
+
+
 def main():
     bookPath = "books/frankenstein.txt"
     book = openBooks(bookPath)
-    wordCount = countBook(book)
-    print(wordCount)
-    
-
+    letterCount = countBook(book)
+    sortedLetterList = sortDict(letterCount)
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    print(f"{wordCount(book)} words found in the document\n")
+    printLetters(sortedLetterList)
+    print(f"--- End report ---")
 
 main()
